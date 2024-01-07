5cea5c1e1053b473dbe1bff6b251ab1e5f4e6b4c
umam17394 <142978439+umam17394@users.noreply.github.com>
2024-01-07 23:22:47 +0530

    Update main.tf

--- task2/modules/ec2/main.tf
+++ task2/modules/ec2/main.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "main" {
               #!/bin/bash
               sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
               sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-              yum install fontconfig java-17-openjdk
+              yum install java-11-amazon-corretto.x86_64 -y
               yum install jenkins
               systemctl start jenkins
               EOF
